d56423dcb38239e589edf51a0a5825c552608317
Peter Pham <pttp15122002@gmail.com>
2024-11-22 13:56:40 +0700

    remove auth token

--- src/env.ts
+++ src/env.ts
@@ -16,16 +16,8 @@ const EnvSchema = z.object({
   PORT: z.coerce.number().default(9999),
   LOG_LEVEL: z.enum(["fatal", "error", "warn", "info", "debug", "trace", "silent"]),
   DATABASE_URL: z.string().url(),
-  DATABASE_AUTH_TOKEN: z.string().optional(),
 }).superRefine((input, ctx) => {
-  if (input.NODE_ENV === "production" && !input.DATABASE_AUTH_TOKEN) {
-    ctx.addIssue({
-      code: z.ZodIssueCode.invalid_type,
-      expected: "string",
-      received: "undefined",
-      path: ["DATABASE_AUTH_TOKEN"],
-      message: "Must be set when NODE_ENV is 'production'",
-    });
+  if (input.NODE_ENV === "production") {
   }
 });
 
