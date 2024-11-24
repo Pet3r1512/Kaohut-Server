27703965a3e530dcad5cee62b600a86ecd2d5647
Peter Pham <pttp15122002@gmail.com>
2024-11-24 14:09:59 +0700

    ops: use better-auth signup with email and password

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,25 +1,21 @@
-import prisma from "@/db/Prisma";
-import getUser from "@/db/users/getUser";
 import { signUpValidator } from "@/middlewares/signUpValidator";
+import { auth } from "@/utils/auth";
 import { Hono } from "hono";
 
 const authRouter = new Hono()
 authRouter.post("/signup", signUpValidator, async (c) => {
     const {email, password, name, role } = c.var.signUpData
 
+    const response = await auth.api.signUpEmail({
+        body: { email, password, name, role },
+      });
+        
+    if (!response) {
+        return c.json(
+            {message: "Error"}, 400
+        )
     }
 
-    prisma.$connect()
-    await prisma.user.create({
-        data: {
-            email: email,
-            password: password,
-            fullName: fullName,
-            role: "student"
-        }
-    })
-    prisma.$disconnect()
-    
     return c.json(
         {message: "Sign Up done"}, 201
     )
