f1047a682adb3afaa6e9434a73c9277edf5d8a1c
Peter Pham <pttp15122002@gmail.com>
2024-11-27 15:08:02 +0700

    chore: remove middleware handler

--- functions/_middleware.ts
+++
@@ -1,6 +0,0 @@
-import { handleMiddleware } from 'hono/cloudflare-pages'
-
-export const onRequest = handleMiddleware(async (c, next) => {
-    console.log(`You are accessing ${c.req.url}`)
-    await next()
-})
