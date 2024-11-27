854d4f043d0d94950071e5a6a5902ef4cc6d9ed8
Peter Pham <89619124+Pet3r1512@users.noreply.github.com>
2024-11-27 10:52:37 +0700

    #FEAT-003: signin (#11)
    * feat: middleware for signin credentials
    
    * chore: format file
    
    * relocate file
    
    * feat: signin route
    
    * chore: format file
    
    * chore
    
    * deployment setup
    
    * feat: cloudflare middleware setup

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,24 +1,40 @@
-import { signUpValidator } from "@/middlewares/signUpValidator";
+import { signInValidator } from "@/middlewares/auth/signin";
+import { signUpValidator } from "@/middlewares/auth/signup";
 import { auth } from "@/utils/auth";
 import { Hono } from "hono";
 
 const authRouter = new Hono()
 authRouter.post("/signup", signUpValidator, async (c) => {
-    const {email, password, name, role } = c.var.signUpData
+    const { email, password, name, role } = c.var.signUpData
 
     const response = await auth.api.signUpEmail({
         body: { email, password, name, role },
-      });
-        
+    });
+
     if (!response) {
         return c.json(
-            {message: "Error"}, 400
+            { message: "Error" }, 400
         )
     }
 
     return c.json(
-        {message: "Sign Up done"}, 201
+        { message: "Sign Up done" }, 201
     )
+})
+
+authRouter.post("/signin", signInValidator, async (c) => {
+    const { email, password } = c.var.signInData
+    const response = await auth.api.signInEmail({
+        body: { email, password }
+    })
+
+    if (!response) {
+        return c.json({ message: "Error" }, 400)
+    }
+
+    return c.json({
+        message: "Sign In Done"
+    }, 201)
 })
 
 export default authRouter;
