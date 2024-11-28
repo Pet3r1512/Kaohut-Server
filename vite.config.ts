a806e840d5be31b1029d4057f226a45a963ba989
Peter Pham <pttp15122002@gmail.com>
2024-11-28 12:57:35 +0700

    fix bugs

--- vite.config.ts
+++ vite.config.ts
@@ -3,21 +3,10 @@ import devServer from '@hono/vite-dev-server'
 import { defineConfig } from 'vite'
 
 export default defineConfig(({ mode }) => {
-  const commonConfig = {
-    build: {
-      rollupOptions: {
-        external: ['.prisma/client/index-browser'],
-      },
-    },
-  }
-
   if (mode === 'client') {
     return {
-      ...commonConfig,
       build: {
-        ...commonConfig.build,
         rollupOptions: {
-          ...commonConfig.build.rollupOptions,
           input: './src/client.ts',
           output: {
             entryFileNames: 'static/client.js',
@@ -27,7 +16,6 @@ export default defineConfig(({ mode }) => {
     }
   } else {
     return {
-      ...commonConfig,
       plugins: [
         pages(),
         devServer({
