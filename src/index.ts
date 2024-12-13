346d819638d390daabc2d8cb60c1d89614b4d530
Peter Pham <pttp15122002@gmail.com>
2024-12-13 14:08:04 +0700

    ops: use trpc routes

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,20 @@ import app from "./app";
 import env from "./env";
 import { auth } from "./utils/auth";
 import authRouter from "./routes/auth";
+import { trpcServer } from "@hono/trpc-server";
+import { appRouter } from "./routes/_app";
 
 const port = env.PORT;
-// eslint-disable-next-line no-console
 console.log(`Server is running on port http://localhost:${port}`);
 
-app.route("/api/auth", authRouter);
+app.use(
+  "/trpc/*",
+  trpcServer({
+    router: appRouter,
+  })
+);
+
+// app.route("/api/auth", authRouter);
 
 app.post('/api/auth/*', (c) => auth.handler(c.req.raw));
 
