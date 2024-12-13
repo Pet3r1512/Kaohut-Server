c421e6a379d9c5ba23599a0993423450c15e6f9b
Peter Pham <pttp15122002@gmail.com>
2024-12-13 14:07:40 +0700

    ops: signup route

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,40 +1,67 @@
-import { signInValidator } from "@/middlewares/auth/signin";
-import { signUpValidator } from "@/middlewares/auth/signup";
 import { auth } from "@/utils/auth";
-import { Hono } from "hono";
+import { publicProcedure, router } from "./tRPC";
+import { z } from "zod"
+import { signUpValidator } from "@/middlewares/auth/signup";
 
-const authRouter = new Hono()
-authRouter.post("/signup", signUpValidator, async (c) => {
-    const { email, password, name, role } = c.var.signUpData
-
-    const response = await auth.api.signUpEmail({
-        body: { email, password, name, role },
-    });
-
-    if (!response) {
-        return c.json(
-            { message: "Error" }, 400
-        )
-    }
-
-    return c.json(
-        { message: "Sign Up done" }, 201
-    )
-})
-
-authRouter.post("/signin", signInValidator, async (c) => {
-    const { email, password } = c.var.signInData
-    const response = await auth.api.signInEmail({
-        body: { email, password }
+export const authRouter = router({
+    signUp: publicProcedure
+        .input(z.object({
+            email: z.string(),
+            password: z.string(),
+            name: z.string(),
+            role: z.string()
+        }))
+        .use(signUpValidator)
+        .mutation(
+            async ({ input }) => {
+                const { email, password, name, role } = input
+                const response = await auth.api.signUpEmail({
+                    body: { email, password, name, role }
+                })
+                if (!response) {
+                    return { status: 400, message: "Error" }
+                }
+                return { status: 201, message: "Sign Up Done" }
+            }
+        ),
+    test: publicProcedure.query(() => {
+        return { messgae: "Test" }
     })
 
-    if (!response) {
-        return c.json({ message: "Error" }, 400)
-    }
-
-    return c.json({
-        message: "Sign In Done"
-    }, 201)
 })
+
+// const authRouter = new Hono()
+// authRouter.post("/signup", signUpValidator, async (c) => {
+//     const { email, password, name, role } = c.var.signUpData
+
+//     const response = await auth.api.signUpEmail({
+//         body: { email, password, name, role },
+//     });
+
+//     if (!response) {
+//         return c.json(
+//             { message: "Error" }, 400
+//         )
+//     }
+
+//     return c.json(
+//         { message: "Sign Up done" }, 201
+//     )
+// })
+
+// authRouter.post("/signin", signInValidator, async (c) => {
+//     const { email, password } = c.var.signInData
+//     const response = await auth.api.signInEmail({
+//         body: { email, password }
+//     })
+
+//     if (!response) {
+//         return c.json({ message: "Error" }, 400)
+//     }
+
+//     return c.json({
+//         message: "Sign In Done"
+//     }, 201)
+// })
 
 export default authRouter;
