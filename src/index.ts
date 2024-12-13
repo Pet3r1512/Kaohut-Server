73ef8a9e1c3997bfc04645622a7d49ac64344180
Peter Pham <89619124+Pet3r1512@users.noreply.github.com>
2024-12-13 14:27:36 +0700

    #OPS-003: install trpc (#16)
    * instal trpc
    
    * fix: missing prisma client
    
    * ops: use 1 instance of prisma
    
    * ops: trpc context
    
    * ops: trpc config
    
    * ops: add auth router to trpc app router
    
    * ops: signup route
    
    * ops: middleware to check sign up credentials
    
    * ops: use trpc routes
    
    * chore: remove unused
    
    * imp: add validator for inputs
    
    * ops: sign in procedure by trpc
    
    * chore: remove unused

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,18 @@ import { serve } from "@hono/node-server";
 import app from "./app";
 import env from "./env";
 import { auth } from "./utils/auth";
-import authRouter from "./routes/auth";
+import { trpcServer } from "@hono/trpc-server";
+import { appRouter } from "./routes/_app";
 
 const port = env.PORT;
-// eslint-disable-next-line no-console
 console.log(`Server is running on port http://localhost:${port}`);
 
-app.route("/api/auth", authRouter);
-
-app.post('/api/auth/*', (c) => auth.handler(c.req.raw));
+app.use(
+  "/trpc/*",
+  trpcServer({
+    router: appRouter,
+  })
+);
 
 // Should remove this later on
 app.get("/", async (c) => {
