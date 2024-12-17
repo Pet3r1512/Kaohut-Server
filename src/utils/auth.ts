ddad520d31fa044f9ce75127d8b0d5472b593f70
Peter Pham <89619124+Pet3r1512@users.noreply.github.com>
2024-12-17 09:29:35 +0700

    #IMP-001: add user workplace (#19)
    * imp: add workplace for user schema
    
    * chore: generated prisma schema from better-auth

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,22 +1,26 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { PrismaClient } from "@prisma/client";
- 
+
 const prisma = new PrismaClient();
- 
+
 export const auth = betterAuth({
-    user: {
-      additionalFields: {
-        role: {
-          type: "string",
-          required: true,
-        }
+  user: {
+    additionalFields: {
+      role: {
+        type: "string",
+        required: true,
+      },
+      workplace: {
+        type: "string",
+        required: true
       }
-    },
-    database: prismaAdapter(prisma, {
-      provider: "postgresql"
-    }),
-    emailAndPassword: { 
-        enabled: true, 
-    },
+    }
+  },
+  database: prismaAdapter(prisma, {
+    provider: "postgresql"
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
 })
