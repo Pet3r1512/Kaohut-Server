ed120330e6acdc14ed962c15530c166d4877ef30
Peter Pham <pttp15122002@gmail.com>
2024-12-19 17:57:01 +0700

    chore: remove unused

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,6 @@ app.use(
   cors({
     origin: CLIENT_URL,
     allowHeaders: ["Content-Type", "Authorization"],
-    allowMethods: ["POST", "GET", "OPTIONS"],
     exposeHeaders: ["Content-Length"],
     maxAge: 600,
     credentials: true,
