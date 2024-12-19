5107c65cec09a3bc32ea71c583d80a009ed4db69
Peter Pham <pttp15122002@gmail.com>
2024-12-19 10:39:59 +0700

    ops: use better-auth routes

--- src/index.ts
+++ src/index.ts
@@ -5,23 +5,31 @@ import env from "./env";
 import { trpcServer } from "@hono/trpc-server";
 import { appRouter } from "./routes/_app";
 import { cors } from "hono/cors";
+import { auth } from "./utils/auth";
 
 const port = env.PORT;
 console.log(`Server is running on port http://localhost:${port}`);
 
-app.use("/*", cors());
-
 app.use(
-  "/trpc/*",
-  trpcServer({
-    router: appRouter,
+  "/api/auth/**", // or replace with "*" to enable cors for all routes
+  cors({
+    origin: "http://localhost:5173", // replace with your origin
+    allowHeaders: ["Content-Type", "Authorization"],
+    allowMethods: ["POST", "GET", "OPTIONS"],
+    exposeHeaders: ["Content-Length"],
+    maxAge: 600,
+    credentials: true,
   })
 );
 
-// Should remove this later on
-app.get("/", async (c) => {
-  return c.text("Hello Hono");
-});
+app.on(["POST", "GET"], "/api/auth/**", (c) => auth.handler(c.req.raw));
+
+// app.use(
+//   "/trpc/*",
+//   trpcServer({
+//     router: appRouter,
+//   })
+// );
 
 serve({
   fetch: app.fetch,
