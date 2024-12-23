7802387716176cc5408fd81635eeb477dfa18a91
Peter Pham <pttp15122002@gmail.com>
2024-12-23 08:52:42 +0700

    ops: add user router to app router

--- src/routes/_app.ts
+++ src/routes/_app.ts
@@ -1,8 +1,11 @@
 import authRouter from './auth';
 import { publicProcedure, router } from './tRPC';
+import { router } from './tRPC';
+import userRouter from './user';
 
 export const appRouter = router({
-    auth: authRouter
+    auth: authRouter,
+    user: userRouter
 });
 
 // Export only the type of a router!
