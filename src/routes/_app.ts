d882834e8708be0a8e9c3f9f25d3c770cd5a27b9
Peter Pham <pttp15122002@gmail.com>
2024-12-30 10:20:44 +0700

    ops: add quiz router to trpc app router

--- src/routes/_app.ts
+++ src/routes/_app.ts
@@ -1,8 +1,10 @@
 import authRouter from './auth';
+import quizRouter from './quiz';
 import { router } from './tRPC';
 
 export const appRouter = router({
     auth: authRouter,
+    quiz: quizRouter
 });
 
 export type AppRouter = typeof appRouter;
