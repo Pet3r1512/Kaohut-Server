b72fe49b202623474b6210eebdd70602412f49b7
Peter Pham <pttp15122002@gmail.com>
2024-12-30 09:39:26 +0700

    chore: remove unused

--- src/routes/_app.ts
+++ src/routes/_app.ts
@@ -1,10 +1,8 @@
 import authRouter from './auth';
 import { router } from './tRPC';
-import userRouter from './user';
 
 export const appRouter = router({
     auth: authRouter,
-    user: userRouter
 });
 
 export type AppRouter = typeof appRouter;
