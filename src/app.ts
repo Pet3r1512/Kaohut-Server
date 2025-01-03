b2309bc7ee0ce69722d4d1d3f7afa8cec56a2e89
Peter Pham <pttp15122002@gmail.com>
2025-01-03 09:41:03 +0700

    remove unused

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,6 @@
 import { Hono } from "hono";
 import { auth } from "./utils/auth";
 
-const app = new Hono<{
-    Variables: {
-        user: typeof auth.$Infer.Session.user | null;
-        session: typeof auth.$Infer.Session.session | null
-    }
-}>();
+const app = new Hono();
 
 export default app;
