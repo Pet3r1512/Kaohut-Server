f278aeddc1765fe0d52f0faf477f3ae7aa013564
Peter Pham <pttp15122002@gmail.com>
2025-01-03 09:29:39 +0700

    fix: missing path from url

--- src/index.ts
+++ src/index.ts
@@ -30,7 +30,7 @@ app.use(
 
 app.on(["POST", "GET"], "/api/auth/**", (c) => auth.handler(c.req.raw));
 
-app.get("/session", async (c) => {
+app.get("/api/auth/get-session", async (c) => {
   const session = c.get("session")
   const user = c.get("user")
 
