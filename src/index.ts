ed0a093e4de518e012f7641efdf7e541dd49a459
Peter Pham <pttp15122002@gmail.com>
2025-01-03 09:35:02 +0700

    try: adding cors

--- src/index.ts
+++ src/index.ts
@@ -30,7 +30,13 @@ app.use(
 
 app.on(["POST", "GET"], "/api/auth/**", (c) => auth.handler(c.req.raw));
 
-app.get("/api/auth/get-session", async (c) => {
+app.get("/api/auth/get-session", cors({
+  origin: CLIENT_URL,
+  allowHeaders: ["Content-Type", "Authorization"],
+  exposeHeaders: ["Content-Length"],
+  maxAge: 600,
+  credentials: true,
+}), async (c) => {
   const session = c.get("session")
   const user = c.get("user")
 
