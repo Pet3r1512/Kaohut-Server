bce4e1aefeb7bce2082f1b76dd5c27a563a6cbbe
Peter Pham <pttp15122002@gmail.com>
2025-01-05 11:01:52 +0700

    ops: add user route to app route

--- src/routes/_app.ts
+++ src/routes/_app.ts
@@ -1,10 +1,12 @@
 import authRouter from './auth';
 import quizRouter from './quiz';
+import userRouter from './user';
 import { router } from './tRPC';
 
 export const appRouter = router({
     auth: authRouter,
-    quiz: quizRouter
+    quiz: quizRouter,
+    user: userRouter
 });
 
 export type AppRouter = typeof appRouter;
