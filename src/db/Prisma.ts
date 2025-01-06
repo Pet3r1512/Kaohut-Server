a9ac1ca1c973bb56f2a60294817e2da90db33e25
Peter Pham <pttp15122002@gmail.com>
2025-01-06 19:50:31 +0700

    fix: invalid prisma client

--- src/db/Prisma.ts
+++ src/db/Prisma.ts
@@ -1,12 +1,8 @@
-/* eslint-disable unicorn/filename-case */
 import { PrismaClient } from "@prisma/client";
 
-// eslint-disable-next-line import/no-mutable-exports
 let prisma: PrismaClient;
 
-// eslint-disable-next-line no-restricted-globals
 if (typeof global != "undefined") {
-  // eslint-disable-next-line no-restricted-globals
   const globalWithPrisma = global as typeof global & { prisma?: PrismaClient };
 
   if (!globalWithPrisma.prisma) {
