3a33a6b5340de5a97344676f02fd59ab600f95b6
Peter Pham <89619124+Pet3r1512@users.noreply.github.com>
2025-01-22 10:03:40 +0700

    #OPS-016: history model (#31)
    * chore: update prisma
    
    * ops: add new history model with needed relations
    
    * chore: update prisma
    
    * ops: new routes for history table
    
    * ops: add history route to app

--- src/routes/_app.ts
+++ src/routes/_app.ts
@@ -2,11 +2,13 @@ import authRouter from './auth';
 import quizRouter from './quiz';
 import userRouter from './user';
 import { router } from './tRPC';
+import historyRouter from './history';
 
 export const appRouter = router({
     auth: authRouter,
     quiz: quizRouter,
-    user: userRouter
+    user: userRouter,
+    history: historyRouter
 });
 
 export type AppRouter = typeof appRouter;
---
+++ src/routes/history.ts
@@ -0,0 +1,39 @@
+import { z } from "zod";
+import { publicProcedure, router } from "./tRPC";
+import prisma from "@/db/Prisma";
+
+const historyRouter = router({
+    createHistory: publicProcedure
+        .input(
+            z.object({
+                userId: z.string(),
+                quizId: z.string(),
+                quizName: z.string(),
+                score: z.number(),
+            })
+        )
+        .mutation(async ({ input }) => {
+            const { userId, quizId, quizName, score } = input
+
+            const newHistory = await prisma.history.create({
+                data: {
+                    userId,
+                    quizId,
+                    quizName,
+                    score,
+                }
+            })
+
+            if (!newHistory) {
+                throw new Error("Cannot add record")
+            }
+
+            return {
+                status: 201,
+                message: "New history added",
+                history: newHistory
+            }
+        })
+})
+
+export default historyRouter
