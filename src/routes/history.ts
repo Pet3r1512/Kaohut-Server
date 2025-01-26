f222d719590829865886bca5bcab9c4814f86887
Peter Pham <89619124+Pet3r1512@users.noreply.github.com>
2025-01-26 12:01:40 +0700

    #OPS-017: User history route (#32)
    * ops: new procedure to get all user's histories
    
    * imp: get 10 records each time
    
    * imp: order results by played time

--- src/routes/history.ts
+++ src/routes/history.ts
@@ -33,6 +33,32 @@ const historyRouter = router({
                 message: "New history added",
                 history: newHistory
             }
+        }),
+    getHistory: publicProcedure.input(z.object({
+        userId: z.string()
+    }))
+        .mutation(async ({ input }) => {
+            const { userId } = input
+
+            const histories = await prisma.history.findMany({
+                where: {
+                    userId: userId
+                },
+                take: 10,
+                orderBy: {
+                    playedAt: "desc"
+                }
+            })
+
+            if (!histories) {
+                throw new Error("Cannot get history")
+            }
+
+            return {
+                status: 200,
+                message: "All histories found",
+                histories: histories
+            }
         })
 })
 
